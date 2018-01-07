cbd6dbc57b18eb3c9dfdbf24a2803e7f26ef6edc
github username <github email address>
2018-01-07 14:43:49 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,9 @@
-class Application
+class Application #The above is the code that will be run whenever there is a request received.
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
 end
-
